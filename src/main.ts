bbeb8f05ce0e07291748348ca9225efb59ba5ead
anderpang <anderpang@qq.com>
2020-02-29 18:33:02 +0800

    简单模板

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 
 let app:HTMLDivElement|null=document.querySelector("#app");
 
-let str:string="Hello World";
+let str:string="<img width=\"64\" src=\"static/wuweierwei.png\" alt=\"\" /><br />Hello World";
 
 app!.style.cssText="text-align:center;"
 app!.innerHTML=str;
